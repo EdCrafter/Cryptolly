e2d178fb228d9110ac467fd27a3d46f2aa8bef39
root <verdev4az@gmail.com>
2023-10-13 10:21:57 +0300

    title

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/css?family=Sofia:regular" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Inter:,regular,medium" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Sora:bold" rel="stylesheet" />
 </head>
 <body>
     <?php
