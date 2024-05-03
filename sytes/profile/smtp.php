99d9fe053c1a2f680b4adcdc7e48ae1d5c4c8297
root <verdev4az@gmail.com>
2024-05-03 21:44:11 +0300

    hide pass

--- sytes/profile/smtp.php
+++ sytes/profile/smtp.php
@@ -5,6 +5,7 @@ use PHPMailer\PHPMailer\Exception;
 require 'PHPMailer/src/Exception.php'; 
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
+$config = require 'config.php';
 
 // Инициализация объекта PHPMailer
 $mail = new PHPMailer(true);
@@ -15,7 +16,7 @@ try {
     $mail->Host       = 'smtp.gmail.com'; // SMTP сервер
     $mail->SMTPAuth   = true;
     $mail->Username   = 'verdev4az@gmail.com'; // Ваше имя пользователя SMTP
-    $mail->Password   = 'hmdo ubsz wlok xtxp';     // Ваш пароль SMTP
+    $mail->Password = $config['smtp_password'];     // Ваш пароль SMTP
     $mail->SMTPSecure = 'tls';              // Используйте ssl или tls
     $mail->Port       = 587;                // Порт SMTP сервера
 
